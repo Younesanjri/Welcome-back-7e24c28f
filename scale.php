1716102f18772f7a9117b68a9e1bf96e39384447
Younes el Anjri <younes@bit-academy.nl>
2020-02-03 12:19:37 +0100

    fixed some stuff

--- scale.php
+++ scale.php
@@ -2,8 +2,8 @@
 if (!$argv[1] && !$argv[2]) {
     print("Please enter 2 numbers to make this working....");
     exit();
-} elseif (count($argv) !== 3 | $argv[1] < 0 | $argv[2] < 0) {
-    print("Number of arguments is incorrect or negative numbers where used..");
+} elseif (count($argv) !== 3 | $argv[1] < 0 | $argv[2] < 0 | is_numeric($argv[1]) + is_numeric($argv[2]) !== 2 ) {
+    print("Number of arguments is incorrect or negative numbers where used or you didn't enter a number");
 }
 
 if ($argv[1] === $argv[2]) {
